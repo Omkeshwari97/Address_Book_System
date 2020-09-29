f7796986930af95331cfb47c9c9fec2c2329299a
Omkeshwari Pilare <omkeshwari1997@gmail.com>
2020-09-29 15:31:23 +0530

    UC1 commit

--- AddressBookMain.java
+++ AddressBookMain.java
@@ -2,29 +2,16 @@ import java.util.*;
 
 public class AddressBookMain
 {
-    private String firstName;
-    private String lastName;
-    private String address;
-    private String city;
-    private String state;
-    private String zip;
-    private String phoneNumber;
-    private String email;
-
-    public AddressBookMain(String firstName,String lastName,String address,String city,String state,String zip,String phoneNumber,String email)
-    {
-        this.firstName=firstName;
-        this.lastName=lastName;
-        this.address=address;
-        this.city=city;
-        this.state=state;
-        this.zip=zip;
-        this.phoneNumber=phoneNumber;
-        this.email=email;
-    }
-
     public static void main(String[] args)
     {
-        AddressBookMain contact1=new AddressBookMain("Omkeshwari","Pilare","Matunga","Mumbai","Maharashtra","400022","987654321","omp@gmail.com");
+        Contact contact1=new Contact("Omkeshwari","Pilare","Matunga","Mumbai","Maharashtra","400022","987654321","omp@gmail.com");
+        System.out.println("Fisrt Name: "+contact1.getFirstName());
+        System.out.println("Last Name: "+contact1.getLastName());
+        System.out.println("Address: "+contact1.getAddress());
+        System.out.println("City: "+contact1.getCity());
+        System.out.println("State: "+contact1.getState());
+        System.out.println("Zip: "+contact1.getZip());
+        System.out.println("Phone Number: "+contact1.getPhoneNumber());
+        System.out.println("Email: "+contact1.getEmail());
     }
 }
