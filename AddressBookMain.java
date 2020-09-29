f515b512d3ae299e334875c6406248329cc78996
Omkeshwari Pilare <omkeshwari1997@gmail.com>
2020-09-29 15:41:27 +0530

    UC2 commit

--- AddressBookMain.java
+++ AddressBookMain.java
@@ -4,14 +4,24 @@ public class AddressBookMain
 {
     public static void main(String[] args)
     {
+        ArrayList<Contact> contactList=new ArrayList<Contact>();
+
         Contact contact1=new Contact("Omkeshwari","Pilare","Matunga","Mumbai","Maharashtra","400022","987654321","omp@gmail.com");
-        System.out.println("Fisrt Name: "+contact1.getFirstName());
-        System.out.println("Last Name: "+contact1.getLastName());
-        System.out.println("Address: "+contact1.getAddress());
-        System.out.println("City: "+contact1.getCity());
-        System.out.println("State: "+contact1.getState());
-        System.out.println("Zip: "+contact1.getZip());
-        System.out.println("Phone Number: "+contact1.getPhoneNumber());
-        System.out.println("Email: "+contact1.getEmail());
+        contactList.add(contact1);
+        Contact contact2=new Contact("Madhuri","Pilare","Vashi","Mumbai","Maharashtra","400044","9878761234","madz@gmail.com");
+        contactList.add(contact2);
+        
+        for(Contact lobj:contactList)
+        {    
+            System.out.println("Fisrt Name: "+lobj.getFirstName());
+            System.out.println("Last Name: "+lobj.getLastName());
+            System.out.println("Address: "+lobj.getAddress());
+            System.out.println("City: "+lobj.getCity());
+            System.out.println("State: "+lobj.getState());
+            System.out.println("Zip: "+lobj.getZip());
+            System.out.println("Phone Number: "+lobj.getPhoneNumber());
+            System.out.println("Email: "+lobj.getEmail());
+            System.out.println("--------------------------------");
+        }
     }
 }
