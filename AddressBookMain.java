65d306518ff45ad69cf244414d0f661e124b9ab7
Omkeshwari Pilare <omkeshwari1997@gmail.com>
2020-09-29 15:53:07 +0530

    UC2 commit

--- AddressBookMain.java
+++ AddressBookMain.java
@@ -5,15 +5,41 @@ public class AddressBookMain
     public static void main(String[] args)
     {
         ArrayList<Contact> contactList=new ArrayList<Contact>();
+        Scanner sc=new Scanner(System.in);
 
-        Contact contact1=new Contact("Omkeshwari","Pilare","Matunga","Mumbai","Maharashtra","400022","987654321","omp@gmail.com");
+        String firstName, lastName, address, city, state, zip, phoneNumber, email;
+
+        System.out.println("Enter First Name:");
+        firstName=sc.next();
+
+        System.out.println("Enter Last Name:");
+        lastName=sc.next();
+        
+        System.out.println("Enter Address:");
+        address=sc.next();
+
+        System.out.println("Enter City:");
+        city=sc.next();
+
+        System.out.println("Enter State:");
+        state=sc.next();
+
+        System.out.println("Enter Zip:");
+        zip=sc.next();
+
+        System.out.println("Enter Phone Number:");
+        phoneNumber=sc.next();
+
+        System.out.println("Enter Email:");
+        email=sc.next();
+
+        Contact contact1=new Contact(firstName,lastName,address,city,state,zip,phoneNumber,email);
         contactList.add(contact1);
-        Contact contact2=new Contact("Madhuri","Pilare","Vashi","Mumbai","Maharashtra","400044","9878761234","madz@gmail.com");
-        contactList.add(contact2);
         
         for(Contact lobj:contactList)
-        {    
-            System.out.println("Fisrt Name: "+lobj.getFirstName());
+        {   
+            System.out.println("--------------------------------");
+            System.out.println("First Name: "+lobj.getFirstName());
             System.out.println("Last Name: "+lobj.getLastName());
             System.out.println("Address: "+lobj.getAddress());
             System.out.println("City: "+lobj.getCity());
